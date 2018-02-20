5604c2070e3f351c8d9070c4b78317918fae3c93
Unknown <ebru>
2018-02-20 15:44:25 +0100

    autoEntrepreneurCPP

---
+++ AppObjetVS/AutoEntrepreneur.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Autoentrepreneur.h"
+using namespace std;
+
+#pragma region Constructeur / Destructeur
+AutoEntrepreneur::AutoEntrepreneur()
+{
+}
+
+
+AutoEntrepreneur::~AutoEntrepreneur()
+{
+}
+
+
+#pragma endregion 
+
+
+#pragma region Get/Set Sirens
+int AutoEntrepreneur::getSirens()
+{
+	return _Sirens;
+}
