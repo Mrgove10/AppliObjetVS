9101f4214c9a828c2e2fa39a6e2fcce200f20fec
Mrgove10 <richaradrien0@gmail.com>
2018-02-20 13:31:04 +0100

    Ajout Classe prof + PDF

---
+++ AppObjetVS/Prof.h
@@ -0,0 +1,18 @@
+#pragma once
+class Prof
+{
+#pragma region Declarations publiques
+public:
+	Prof();
+	~Prof();
+
+#pragma endregion
+
+#pragma region Declarations privées
+private:
+
+
+#pragma endregion
+
+};
+
