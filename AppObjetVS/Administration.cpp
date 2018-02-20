fa8abc8b85e60940c539e8a9628762ecac211246
LouBege <wolfla@hotmail.fr>
2018-02-20 14:50:35 +0100

    Modif Admin

--- AppObjetVS/Administration.cpp
+++ AppObjetVS/Administration.cpp
@@ -10,3 +10,153 @@ Administration::Administration()
 Administration::~Administration()
 {
 }
+
+string Administration::getNom()
+{
+	return m_nom;
+}
+
+void Administration::setNom(string nomPara)
+{
+	m_nom = nomPara;
+}
+
+string Administration::getPrenom()
+{
+	return m_prenom;
+}
+
+void Administration::setPrenom(string prenomPara)
+{
+	m_prenom = prenomPara;
+}
+
+string Administration::getEmail()
+{
+	return m_email;
+}
+
+void Administration::setEmail(string emailPara)
+{
+	m_email = emailPara;
+}
+
+int Administration::getTelephone()
+{
+	return m_telephone;
+}
+
+void Administration::setTelephone(int telephonePara)
+{
+	m_telephone = telephonePara;
+}
+
+int Administration::getDateNaissance()
+{
+	return m_dateNaissance;
+}
+
+void Administration::setDateNaissance(int dateNaissancePara)
+{
+	m_dateNaissance = dateNaissancePara;
+}
+
+int Administration::getNumCompteBancaire()
+{
+	return m_numCompteBancaire;
+}
+
+void Administration::setNumCompteBancaire(int numCompteBancairePara)
+{
+	m_numCompteBancaire = numCompteBancairePara;
+}
+
+string Administration::getEmailPro()
+{
+	return m_emailPro;
+}
+
+void Administration::setEmailPro(string emailProPara)
+{
+	m_emailPro = emailProPara;
+}
+
+int Administration::getTelFixPro()
+{
+	return m_telFixPro;
+}
+
+void Administration::setTelFixPro(int telFixProPara)
+{
+	m_telFixPro = telFixProPara;
+}
+
+int Administration::getTelMobilePro()
+{
+	return m_telMobilePro;
+}
+
+void Administration::setTelMobilePro(int telMobilePara)
+{
+	m_telMobilePro = telMobilePara;
+}
+
+float Administration::getHoraires()
+{
+	return m_horaires;
+}
+
+void Administration::setHoraires(float horairesPara)
+{
+	m_horaires = horairesPara;
+}
+
+string Administration::getContrat()
+{
+	return m_contrat;
+}
+
+void Administration::setContrat(string contratPara)
+{
+	m_contrat = contratPara;
+}
+
+string Administration::getTypeDePoste()
+{
+	return m_typeDePoste;
+}
+
+void Administration::setTypeDePoste(string typeDePostePara)
+{
+	m_typeDePoste = typeDePostePara;
+}
+
+int Administration::getSalaire()
+{
+	return m_salaire;
+}
+
+void Administration::setSalaire(int salairePara)
+{
+	m_salaire = salairePara;
+}
+
+int Administration::getDateVirementSalaire()
+{
+	return m_dateVirementSalaire;
+}
+
+void Administration::setDateVirementSalaire(int dateVirementSalairePara)
+{
+	m_dateVirementSalaire = dateVirementSalairePara;
+}
+
+int Administration::getCongeRestant()
+{
+	return m_congeRestant;
+}
+
+void Administration::setCongeRestant(int congeRestantPara)
+{
+	m_congeRestant = congeRestantPara;
+}
--- AppObjetVS/Administration.h
+++ AppObjetVS/Administration.h
@@ -1,11 +1,76 @@
 #pragma once
 #include <iostream>
 #include <vector>
+
+using namespace std;
+
 class Administration
 {
 public:
 	Administration();
 	~Administration();
+
+	string getNom();
+	void setNom(string nomPara);
+
+	string getPrenom();
+	void setPrenom(string prenomPara);
+
+	string getEmail();
+	void setEmail(string emailPara);
+
+	int getTelephone();
+	void setTelephone(int telephonePara);
+
+	int getDateNaissance();
+	void setDateNaissance(int dateNaissancePara);
+
+	int getNumCompteBancaire();
+	void setNumCompteBancaire(int numCompteBancairePara);
+
+	string getEmailPro();
+	void setEmailPro(string emailProPara);
+
+	int getTelFixPro();
+	void setTelFixPro(int telFixProPara);
+
+	int getTelMobilePro();
+	void setTelMobilePro(int telMobilePara);
+
+	float getHoraires();
+	void setHoraires(float horairesPara);
+
+	string getContrat();
+	void setContrat(string contratPara);
+
+	string getTypeDePoste();
+	void setTypeDePoste(string typeDePostePara);
+
+	int getSalaire();
+	void setSalaire(int salairePara);
+
+	int getDateVirementSalaire();
+	void setDateVirementSalaire(int dateVirementSalairePara);
+
+	int getCongeRestant();
+	void setCongeRestant(int congeRestantPara);
+
+private:
+	string m_nom;
+	string m_prenom;
+	string m_email;
+	int m_telephone;
+	int m_dateNaissance;
+	int m_numCompteBancaire;
+	string m_emailPro;
+	int m_telFixPro;
+	int m_telMobilePro;
+	int m_horaires;
+	string m_contrat;
+	string m_typeDePoste;
+	int m_salaire;
+	int m_dateVirementSalaire;
+	int m_congeRestant;
 
 };
 
