3cb4859a1d72d2a37aa7db55a51fcc6f5219983e
LouBege <wolfla@hotmail.fr>
2018-02-20 14:00:28 +0100

    Modif Prof

--- AppObjetVS/Prof.h
+++ AppObjetVS/Prof.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Prof
 {
 #pragma region Declarations publiques
@@ -6,11 +11,43 @@ public:
 	Prof();
 	~Prof();
 
+	string getNom();
+	void setNom(string nomPara);
+
+	string getPrenom();
+	void setPrenom(string prenomPara);
+
+	string getEmail();
+	void setEmail(string emailPara);
+
+	int getTelephone();
+	void setTelephone(int telephonePara);
+
+	int getDateNaissance();
+	void setDateNaissance(int dateNaissancePara);
+
+	int getNumCompteBancaire();
+	void setNumCompteBancaire(int numCompteBancairePara);
+
+	//module?
+
+	string getCasierJudiciaire();
+	void setCasierJudiciare(string casierJudiciairePara);
+
+	
+
 #pragma endregion
 
 #pragma region Declarations privées
 private:
-
+	string m_nom;
+	string m_prenom;
+	string m_email;
+	int m_telephone;
+	int m_dateNaissance;
+	int m_numCompteBancaire;
+	//liste module
+	string m_casierJudiciare;
 
 #pragma endregion
 
