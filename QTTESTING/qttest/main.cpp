e207e6cab81f4d2893e3360c8979b1548d2bcb7f
Mrgove10 <richaradrien0@gmail.com>
2018-02-27 15:50:26 +0100

    remate QT app

--- QTTESTING/qttest/main.cpp
+++ QTTESTING/qttest/main.cpp
@@ -1,16 +1,8 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication a(argc, argv);
 
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-    return app.exec();
+    return a.exec();
 }
