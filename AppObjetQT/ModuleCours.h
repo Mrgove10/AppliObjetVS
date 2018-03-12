5c68131998b589adb6c758ac6d6ff28951511cf8
Adrien Richard <richaradrien0@gmail.com>
2018-03-12 13:41:17 +0100

    added code and removed usedlex using

---
+++ AppObjetQT/ModuleCours.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "RelevéNotes.h"
+#include "RelevéNotes.h"
+using namespace std;
+class RelevéNotes;
+class ModuleCours
+{
+#pragma region Declarations publiques
+public:
+	ModuleCours();
+	ModuleCours(string intitule, string facture, int nbJourEnseignement);
+	~ModuleCours();
+
+	string getIntituler();
+	void setIntituler(string intitulePara);
+
+	string getFacture();
+	void setfacture(string facturePara);
+
+	float getHeuresTotales();
+	void setHeuresTotales(float heuresTotalesPara);
+#pragma endregion 
+
+#pragma region Declarations privées
+private:
+	string _intituleModule;
+	string _facture;
+	float _heuresEnseignees;
+	vector<RelevéNotes*> _notesClasse;
+#pragma endregion 
+};
