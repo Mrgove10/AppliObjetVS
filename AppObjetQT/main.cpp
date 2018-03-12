61669fd2185c8874f091b1a12464277fd520bad7
Mrgove10 <richaradrien0@gmail.com>
2018-03-12 14:04:53 +0100

    sql

--- AppObjetQT/main.cpp
+++ AppObjetQT/main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QtSql/QtSql>
+#include <iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -8,4 +10,32 @@ int main(int argc, char *argv[])
     w.show();
 
     return a.exec();
+
+}
+
+
+void SQL(){
+    QString connectionString = "Driver={ODBC Driver 13 for SQL Server};"
+                               "Server=tcp:appobjet.database.windows.net,1433;"
+                               "Database=Appobjet;"
+                               "Uid=AdrienRichard@appobjet;"
+                               "Pwd="""";"
+                               "Encrypt=yes;"
+                               "TrustServerCertificate=no;"
+                               "Connection Timeout=30;";
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC", mydb);
+    db.setDatabaseName(connectionString);
+
+    if (db.open())
+    {
+        //return true;
+        cout << true<<endl;
+    }
+    else
+    {
+        QString error = db.lastError().text();
+        //return false;
+        cout << false<<endl;
+    }
 }
