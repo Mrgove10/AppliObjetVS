fd884938b52e32ec21b7df092632450bf15d990e
Unknown <ebru_omay@hotmail.fr>
2018-04-17 10:07:14 +0200

    ebru

--- AppObjetQT/AppObjetQT.pro
+++ AppObjetQT/AppObjetQT.pro
@@ -26,7 +26,8 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp
+        mainwindow.cpp \
+    ebru.cpp
 
 HEADERS += \
         mainwindow.h
---
+++ AppObjetQT/ebru.cpp
@@ -0,0 +1,69 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "ModuleCours.h".h"
+#include ".h"
+#include ".h"
+#include <QLineEdit>
+#include <iostream>
+#include <QtSql>
+#include <QDebug>
+#include <QCoreApplication>
+#include <QFileDialog>
+#include <fstream>
+void MainWindow::on_pushButton_clicked()
+{
+    ModuleCours*ModuleCours=new Modulecours(
+                ui ->intitule->text().toStString(),
+
+              ui ->facture -> text().toInt(),
+               ui ->HeuresTotales -> text().toFloat());
+    createCSV(ModuleCoursActuel->getIntitule(), ModuleCoursActuel->geHeuresTotales());
+}
+
+}
+
+
+
+
+void MainWindow::
+void MainWindow ::MainWindow (QWidget*parent) :
+    QDialog (parent),
+    ui (new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->comboBox -> additem("AutoEntrepreuneur")
+    ui->comboBox -> additem("Employe")
+
+
+
+
+ #include <QApplication>
+ #include <QtWidgets>
+
+        int MainWindow(int argc, char *argv[])
+        {
+            QApplication app(argc, argv);
+
+            QWidget fenetre;
+            QGroupBox *groupbox = new QGroupBox("Votre Statut", &fenetre);
+
+            QRadioButton *AutoEntrepreneur = new QRadioButton("Auto Entrepreneur");
+            QRadioButton *Employe = new QRadioButton("Employe");
+
+            steacks->setChecked(true);
+
+            QVBoxLayout *vbox = new QVBoxLayout;
+            vbox->addWidget(AutoEntrepreneur);
+            vbox->addWidget(Employe);
+
+            groupbox->setLayout(vbox);
+            groupbox->move(5, 5);
+
+            fenetre.show();
+
+            return app.exec();
+        }
+
+
+
+}
